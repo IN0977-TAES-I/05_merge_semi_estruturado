8e88dd32e2edd3cb75bc2b51da4d208ded5180ea
Richardson Bruno da Silva Andrade <richardson.andrade@reitoria.ifpe.edu.br>
2026-09-15 23:14:57 -0300

    chores: updates outputs

--- command/005.sh
+++ command/005.sh
@@ -3,6 +3,9 @@ diff3 -m [left] [base] [right]
 
 diff3 -m experiment/002q/left.java experiment/002q/base.java experiment/002q/right.java > experiment/002q/diff3_002q.out
 
+
+diff3 -m experiment/005q/left.java experiment/005q/base.java experiment/005q/right.java > experiment/005q/diff3_005q.out
+
 cargo run -- merge --semistructured=diff3 [base] [left] [right]
-cargo run -- merge --semistructured=diff3 experiment/002q/base.java experiment/002q/left.java experiment/002q/right.java > experiment/002q/mergiraf-semi_002q.out
+cargo run -- merge --semistructured=diff3 experiment/005q/base.java experiment/005q/left.java experiment/005q/right.java > experiment/005q/mergiraf-semi_005q.out
 
--- command/006.sh
+++ command/006.sh
@@ -1,4 +1,6 @@
 
-diff3 -m experiment/003q/left.java experiment/003q/base.java experiment/003q/right.java > experiment/002q/diff3_003q.out
+diff3 -m experiment/006q/left.java experiment/006q/base.java experiment/006q/right.java > experiment/006q/diff3_006q.out
 
 cargo run -- merge --semistructured=diff3 experiment/003q/base.java experiment/003q/left.java experiment/003q/right.java > experiment/003q/mergiraf-semi_003q.out
+
+cargo run -- merge experiment/006q/base.java experiment/006q/left.java experiment/006q/right.java > experiment/006q/mergiraf-semi_006q.out
